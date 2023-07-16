f047d74da1097eb5d2addfa70bedfdbc6e10f1d1
Tuan Anh <132194452+tuananh31j@users.noreply.github.com>
2023-07-16 11:35:46 +0700

    cau hinh du an (#2)

---
+++ pdo.php
@@ -0,0 +1,79 @@
+<?php
+//hàm kết nối database
+    function pdo_get_connection() {
+        $hostname='localhost';
+        $db_name='du_an_mau';
+        $username='root';
+        $password="";
+        // Dựng đối tương PDO
+        
+            $connect=new PDO("mysql:host=$hostname;dbname=$db_name",$username, $password);
+            $connect->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $connect;
+        
+}
+    
+
+//hàm CUD
+    function pdo_execute($sql) {
+        $sql_args = array_slice(func_get_args(), 1);
+        
+        try{
+            $connect = pdo_get_connection();
+            $stmt = $connect->prepare($sql);
+            $stmt->execute($sql_args);
+        }
+        catch(PDOException $e){
+            throw $e;
+        }
+        finally{
+            unset($connect);
+        }
+    }
+
+
+
+
+
+
+
+//hàm query nhiều
+function pdo_query($sql) {
+    $sql_args = array_slice(func_get_args(), 1);
+
+    try{
+        $connect = Pdo_get_connection();
+        $stmt = $connect->prepare($sql);
+        $stmt->execute($sql_args);
+        $data = $stmt->fetchAll();
+        return $data;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($connect);
+    }
+}
+
+
+//hàm query một
+function pdo_query_one($sql) {
+    $sql_args = array_slice(func_get_args(), 1);
+
+    try {
+        $connect = Pdo_get_connection();
+        $stmt = $connect->prepare($sql);
+        $stmt->execute($sql_args);
+        $data = $stmt->fetch();
+        return $data;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($connect);
+    }
+}
+
+?>
---
+++ site/controller/comment.php
@@ -0,0 +1,13 @@
+<?php
+//hàm lấy ra danh sách bình luận
+function getListComments() {
+    
+}
+
+//hàm xóa bình luận
+
+
+//hàm sửa bình luận
+
+
+?>
