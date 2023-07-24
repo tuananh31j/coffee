fefd15055a3af7f80b717a0561fc00d44da17e76
duclhph20070 <duclhph20070@fpt.edu.vn>
2023-07-24 17:39:03 +0700

    signup

--- admin/models/signup.php
+++ admin/models/signup.php
@@ -3,4 +3,9 @@
         $sql="insert into signup(fullname,sdt,email,password) values('$fullname','$sdt','$email','$password')";
         pdo_execute($sql);
     }
+    function checktaikhoan($email,$sdt){
+        $sql="select * from taikhoan where email='".$email."' AND sdt='".$sdt="'";
+        $sp=pdo_query_one($sql);
+        return $sp;
+    }
 ?>
